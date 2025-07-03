9a887863bd6f18c0eceab00b23919519ad0c845a
Jorge Vasquez <minomoreno86@gmail.com>
2025-07-02 19:52:32 -0500

    Implementación de benchmark poblacional comparativo en calculadora de aborto

---
+++ benchmarks.py
@@ -0,0 +1,9 @@
+# benchmarks.py
+
+BENCHMARK_RIESGO_ABORTO = {
+    "Menos de 30": 12,     # 10-15% global
+    "30-34": 15,           # 12-15% global
+    "35-39": 25,           # 20-25% global
+    "40-44": 50,           # 33-50% global
+    "45+": 75              # hasta 75-80% global
+}
